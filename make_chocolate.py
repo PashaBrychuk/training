e4439d677674c879f7e8c7ed16f069070b3761d4
Pasha Brychuk <pashabrychuk@gmail.com>
2017-04-21 14:35:19 +0300

    My own wrong solution to file make_chocolate.py is added as a comment

--- make_chocolate.py
+++ make_chocolate.py
@@ -1,3 +1,23 @@
+
+
+
+""" My dead end solution
+def make_chocolate(small, big, goal):
+  if small+big*5>=goal:
+    if goal%5<=small:
+      if goal%5!=0:
+        return goal%5
+      if goal == big*5:
+        return 0
+  return -1
+
+"""
+
+
+
+
+
+
 def make_chocolate(small, big, goal):
   maxBig = goal / 5
    
