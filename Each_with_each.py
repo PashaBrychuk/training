440a7155f0e8b77156e2ddb2afe6cf08445bffd7
Pasha Brychuk <pashabrychuk@gmail.com>
2017-05-22 18:14:53 +0300

    each_to_each.py is now finished

--- Each_with_each.py
+++ Each_with_each.py
@@ -2,7 +2,7 @@ names = ["Pasha", "Dasha", "Masha"]
 numbers = [1,2,3]
 result_list = []
 for name in range(len(names)):
-	for numbers in range(len(numbers)):
-		result_list.append((name, numbers))
+	for number in range(len(numbers)):
+		result_list.append((name, number))
 
 print result_list
