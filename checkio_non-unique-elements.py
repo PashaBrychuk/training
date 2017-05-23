9892b8547926703e43eab4abf84768e6e397576a
Pasha Brychuk <pashabrychuk@gmail.com>
2017-05-23 18:07:42 +0300

    new checkio_non-unique-elements.py is added and need to be completed at home

---
+++ checkio_non-unique-elements.py
@@ -0,0 +1,19 @@
+"""def checkio(data):
+  return  [x for x in data if data.count(x) >= 2]
+"""
+
+
+def checkio(data):
+	sum = []
+	for i in range(len(data)):
+		for j in range(1, len(data)-i):
+				
+				if data[i] == data[j]:
+					print  "i", i
+					print "j", j
+					sum.append(data[i])
+	return sum
+z = [1,2,3,2,1]
+print checkio(z)
+
+
