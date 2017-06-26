9362f088ca56a414b6af056e63d787780ff06390
Pasha Brychuk <pashabrychuk@gmail.com>
2017-06-26 16:07:13 +0300

    new files are added Connect_sqlite.py among them

--- mutating.py
+++ mutating.py
@@ -36,3 +36,33 @@ for x in range(len(nums)):
 	nums[x] = nums[x]*10
 	print "inside ", nums
 print "outside ", nums
+
+print "*"*25
+
+#append twice good mutates argument
+def append_twice_good(alist, val,za):
+	alist.append(val)
+	alist.append(val)
+	return za
+z = [1,2,3]
+append_twice_good(z, 7, 8)
+print z
+
+print "*"*25
+
+#append twice bad   useless 
+def append_twice_bad(blist, val):
+	blist = blist+[val, val]
+	return
+c = [1,2,3]
+append_twice_bad(c, 7)
+print c
+
+print "-----"*5
+def append_twice_bad_fixed(blist, val):
+	blist = blist+[val, val]
+	print "inside", blist
+	return blist
+g = [1,2,3]
+g = append_twice_bad_fixed(g, 7)
+print g
