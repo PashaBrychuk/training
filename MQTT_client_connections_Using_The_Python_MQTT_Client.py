7b6e179e81219ba4f7cdcf44f1a22c3f4cefb271
PashaBrychuk <pashabrychuk@gmail.com>
2017-06-27 17:14:30 +0300

    Massive changes to MQTT_client_connections_Using_The_Python_MQTT_Client.py

--- MQTT_client_connections_Using_The_Python_MQTT_Client.py
+++ MQTT_client_connections_Using_The_Python_MQTT_Client.py
@@ -1,3 +1,4 @@
+
 #https://www.youtube.com/watch?v=vT4FTRgipOM
 import paho.mqtt.client as mqtt
 import time
@@ -6,6 +7,17 @@ def on_connect(client, userdata, flags, rc):
 		print "Connected OK"
 	print "Bad connection Returned code = ", rc
 broker = "192.168.1.184"
-client = mqtt.Client("Python1")
-for i in client.__dict__:
+client = mqtt.Client("Python1") #create new instance of mqtt
+"""for i in client.__dict__:
 	print i
+"""
+
+client.on_connect = on_connect #bind call function
+print "Connecting to broker", broker
+client.loop_start()  #start loop
+client.connect(broker) #connect to broker
+client.publish("house/main-light",  "off") #publishes "off" message to "house/main-light" topic
+time.sleep(4)
+client.loop_stop()  #stops loop
+client.disconnect() # disconnects
+print "-----"
