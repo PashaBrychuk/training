96d05311580492e29a35d109e3c632d6fc2c2c1a
PashaBrychuk <pashabrychuk@gmail.com>
2017-07-12 15:55:46 +0300

    new file selection_sort.py is now added and needs to be finished

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+#selection_sort.py
+
+
+def selection_sort(nums):
+	#tmp = nums[0]
+	for i in range(1, len(nums)):
+		for j in range(i, len(nums)-i):
+			tmp = nums[0]
+			if nums[i]<tmp:
+				print "!!"
+				tmp = nums[i]
+				nums[i] = tmp
+	return nums
+
+
+n = [3,2,4,7,1, -124,3,8]
+print selection_sort(n)
