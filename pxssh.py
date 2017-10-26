f9d9d08d99d401e9eedcb5074a259d2077c9feb6
PashaBrychuk <pashabrychuk@gmail.com>
2017-10-26 18:37:20 +0300

    pxssh.py file is modified

--- pxssh.py
+++ pxssh.py
@@ -4,8 +4,11 @@ s = pxssh.pxssh()
 #print s
 z = pxssh.pxssh()
 
-print s.login("172.24.223.58", "root","")
-s.logout()
+#print s.login("172.24.223.58", "root","")
+#s.logout()
 s.pid=None
-print s.login("172.24.223.58", "root","")
-print help(s.login)
+print s.login("172.24.223.58", "root","", original_prompt="root@congatec-qa3-64:~#")
+print s.isalive()
+#print help(s.login)
+
+print help(s)
